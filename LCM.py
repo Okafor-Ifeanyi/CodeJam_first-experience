4d1a937714bead00923be4297bc59cad7ebce62e
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2022-09-25 20:59:20 +0100

    LCM Update

--- LCM.py
+++ LCM.py
@@ -1,44 +1,74 @@
-word = "2/4+2/6"
-factor = word.split("+")
-
 num_tot = list()
 den_tot = list()
 
-for value in factor:
-    num = value.split("/")
-    # print(num)
+def fetch_digits(question):
+    factor = question.split("+")
 
-    res = [eval(i) for i in num]
-    
-    if (res[1] % res[0]) == 0:
-        num = res[0] / res[0]
-        den = res[1] / res[0]
+    for value in factor:
+        num = value.split("/")
+        res = [eval(i) for i in num]
+        
+        # if (res[1] % res[0]) == 0:
+        #     num = res[0] / res[0]
+        #     den = res[1] / res[0]
 
-        num_tot.append(num)
-        den_tot.append(den)
-
-    else:
         num_tot.append(res[0])
         den_tot.append(res[1])
 
-print(num_tot)
-print(den_tot)
+    return num_tot, den_tot
+    
+def fetch_LCM(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+    
+    while True:
+        if (greater % x == 0) and (greater % y == 0):
+            lcm = greater
+            break
+        greater += 1
 
-den = den_tot[0] * den_tot[1]
-num1 = (den / den_tot[0]) + num_tot[0]
-num2 = (den / den_tot[1]) + num_tot[1]
+    return lcm
+
+word = input('Enter fraction: ')
+fetch_digits(word)
+
+lcm = fetch_LCM(den_tot[0], den_tot[1])
+lcm = int(lcm)
+num1 = (lcm / den_tot[0]) * num_tot[0]
+num2 = (lcm / den_tot[1]) * num_tot[1]
 
 num = num1 + num2
-print(str(num) +"/"+ str(den))
+num = int(num)
 
-def try_nums(num, den):
-    # for i in list:
-    i = 3
-    if num % i == 0 and den % i == 0:
-        lcm_num = num / i
-        lcm_den = den / i
-        
-        print(lcm_num, lcm_den)
+def reducefract(n, d):
+    '''Reduces fractions. n is the numerator and d the denominator.'''
+    def gcd(n, d):
+        while d != 0:
+            t = d
+            d = n%d
+            n = t
+        return n
+    assert d!=0, "integer division by zero"
+    assert isinstance(d, int), "must be int"
+    assert isinstance(n, int), "must be int"
+    greatest=gcd(n,d)
+    n/=greatest
+    d/=greatest
+    return int(n), int(d)
 
+def get_whole_number(x, y):
+    if x > y:
+        rem = x % y
+        whole = x // y
+        if rem == 0:
+            print(f"{int(whole)}/{int(y)}")
+        else:
+            print(f"{int(whole)} {int(rem)}/{int(y)}")
 
-try_nums(num, den)
+    else:
+        print(str(x) +'/'+ str(y))
+
+rc = reducefract(num, lcm)
+get_whole_number(rc[0], rc[1])
